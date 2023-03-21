6c75e665b02a30b1a10bafbcec3802f30071dcf3
anelise <anelise_kipper@estudantesesi.org.br>
2023-03-21 13:54:58 -0300

    cria cabecalho e linka as pastas

---
+++ src/components/Cabecalho.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Cabecalho() {
+  return (
+    <header className='container-cabecalho'>
+        <span>
+            Loja de Departamentos FuturoDev
+        </span>
+        <nav>
+            <a href='Produtos'> </a>
+            <a href='PesquisarProduto'> </a>
+            <a href='CarrinhodeCompras'> </a>
+            <a href='CupomFiscal'> </a>
+        </nav>
+
+    </header>
+  )
+}
