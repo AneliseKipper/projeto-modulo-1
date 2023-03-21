4260babc43ffc02b6ad5e36249176a0e278f62cd
anelise <anelise_kipper@estudantesesi.org.br>
2023-03-21 15:30:57 -0300

    altera a nav manual para uma nav do bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Cabecalho from './components/Cabecalho' 
 import './App.css';
 
--- src/components/Cabecalho.js
+++ src/components/Cabecalho.js
@@ -1,18 +1,30 @@
+import Nav from 'react-bootstrap/Nav';
 import React from 'react'
 
 export default function Cabecalho() {
   return (
+    <>
     <header className='container-cabecalho'>
         <span>
             Loja de Departamentos FuturoDev
         </span>
-        <nav>
-            <a href='Produtos'> </a>
-            <a href='PesquisarProduto'> </a>
-            <a href='CarrinhodeCompras'> </a>
-            <a href='CupomFiscal'> </a>
-        </nav>
+        
+        <Nav defaultActiveKey="/Setores" as="ul">
+      <Nav.Item as="li">
+        <Nav.Link href="Produtos">Produtos</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link eventKey="Pesquisar Produto">Pesquisar Produto</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link eventKey="Carrinho de Compras">Carrinho de Compras</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link eventKey="Cupom Fiscal">Cupom Fiscal</Nav.Link>
+      </Nav.Item>
+    </Nav>
 
     </header>
+    </>
   )
 }
